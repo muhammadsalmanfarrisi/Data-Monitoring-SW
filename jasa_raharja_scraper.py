6b8df710c3950a4efb7b3fecdbd823836fd431e4
muhammadsalmanfarrisi <muhammadsalman85396@gmail.com>
2024-12-03 13:49:03 +0700

    benerin host

--- jasa_raharja_scraper.py
+++ jasa_raharja_scraper.py
@@ -255,7 +255,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
 # from flask import Flask, render_template, request
 # from datetime import datetime
