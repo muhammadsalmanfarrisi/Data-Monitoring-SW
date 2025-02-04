e46d0726664e2e024cca2064b2ab25d73eedc2c0
muhammadsalmanfarrisi <muhammadsalman85396@gmail.com>
2025-02-04 14:11:47 +0700

    edit selesai 1 kirim vidio mas alan

--- jasa_raharja_scraper.py
+++ jasa_raharja_scraper.py
@@ -5,6 +5,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
 import time
 from flask import session
+from flask import Flask, render_template, request, session
 import os
 
 
@@ -21,13 +22,13 @@ def ambil_data(url, retries=3, timeout=10):
 
     # Menambahkan cookies yang Anda salin dari browser (Ganti dengan cookies yang relevan)
     cookies = {
-        'XSRF-TOKEN': 'eyJpdiI6IlRFVVRiUUwyR0JBRk1tdEdDWVRxZ3c9PSIsInZhbHVlIjoiNGdUNG5oR0ZpcnBjeC9ka2JoMjNVb2dvSGxRVjRxQkxvL0k5M0VhclNOcE91eTZScHk2MUNQcytOckdITWxSMkJFK2F2SGNrY2dHZk9SdjdwV3hiWmUwOW5PaXdpditYbXU0bU12WStpZlF3WUxYMTdNc1daaVc2L2lwOG8yZE0iLCJtYWMiOiI2NzljYTM2NTFkOTQ0ZDI4N2EyMTdiZWE2ZWMxZDBlNGMzZGYzNjI1MTA1ZGNlZjIzYjdhOWQ2OTAwZTVmZGI3IiwidGFnIjoiIn0%3D',
+        'XSRF-TOKEN': 'eyJpdiI6IkhVaDdUc1U2YU9qMHB2ZUtMUkw3aWc9PSIsInZhbHVlIjoiazlHMitYVXpqbUVVbVRFUUU4TkJrS21XTzluQXBKNFZKb2NUc2RLQTkweWVpekhiRFdHWEVvMmJtczRvTVNSc3RtSWlVZjZSZTZHSFhuZFVmS0JyQ2pUYWRRZXNBUnMvdUFkajhCeVhOczI5ZWNiZldLSmZKbm1vU3J3bWYza3QiLCJtYWMiOiJkMjhiMTIxZWY1NzhhMGZiMzg4NGFkNDA1OWUyNWM3ZmI2MjI4YjU1YjU1ZmRhNDMzZGZmNjU4ZTY1YWQ0ZTdlIiwidGFnIjoiIn0%3D',
         '_ga': 'GA1.1.140722331.1727057868',
         '_ga_JQ088T32QP': 'GS1.1.1727061610.2.1.1727061629.0.0.0', 
         '_ga_VNWN27RPNX': 'GS1.3.1727061611.2.0.1727061611.60.0.0', 
-        'ceri_session': 'eyJpdiI6ImRTT2s5bWlGdzVNWmhMVkFnUVU0VlE9PSIsInZhbHVlIjoiSTFCa0NOcTZVN2RjVXRFSnNhc2ZoV3hWTzZxZW1JTFRpS21sUnJ6UldLaitLV1NjR0VkOWE1SENEbEkyTlg5TVUrSDV3TldNclBFbDJ2VEtpblZTZyt3S3M1eEt6bzJ1eG9raWJtVVlKclIyRCs0WFN2VklLU1JmbURISkt6Z1ciLCJtYWMiOiI0M2ZlMDhjNGJiOGUzOWM5MzU5YzJhOTFhOTAyYTc5MmY2MDU3YTEzMjQ4ZjU1N2U1MjVjZjU0OGFmNDhjZjYxIiwidGFnIjoiIn0%3D',
+        'ceri_session': 'eyJpdiI6IjU1UG05WWNYZkVMTDNDWEJiQk5idVE9PSIsInZhbHVlIjoiNDRQWnllVHB0SC8xd3pmQVpBUjcwSUg5bjQ1Q3Q2TElwNXRQcU1FSHRVM2NMSjAybDNydEVDS05sTlZON0pjbzVUNXhWa3d5bG5IV1BMOEtlRjcxRjFkdkdTa1VzQWpNa0w0Nmg3QktKQy9jVWVHL0VNc1dweUdFWjRuekc5SnEiLCJtYWMiOiI4NDQ3ODg2N2RkM2NlNzJiYjc0MTZlZjFkMGM4ZmJjNzRhNWU5MDNmNjQzODRlNGYyYzQ4MGFkM2YyMzk1M2U0IiwidGFnIjoiIn0%3D',
         'cookiesession1': '678B28C4BA1B09254D21278D87A606A5',
-        'jr_cookie': 'c686aabd2eea2ae0eedde6ce6a909c67',
+        'jr_cookie': 'c686aabd2eea2ae0eedde6ce3735a067',
         'remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d': 'eyJpdiI6Ii9wYUR3Qm9aNFh0WEhOUjQ5K2FYbXc9PSIsInZhbHVlIjoiRXVTWkZQNThZTWFoZ0ZBQU41SE1obFVQK0pncW0rQ3EwRHdQbkp0MGE1QmtWZXBxSnAveWNsQ2h3ZzNjWVZXV0pHVUlJNGs3dm1oZTlNSHVGVGRCU0VOK1E3QUVaaS9yNHpPR09abGx3VmZoSVF6dWFJbk51NEZ3eXdHODNwcjRVbXZ1cFRCL0xKdkFYaUxiTTdTSWVRPT0iLCJtYWMiOiJkN2NkMzQ5Mjc3NTVhYmRiNzU4NzBiNmU4MjE2NWQ5NzgwNDE5NWNjYWI2YTFkZTY1MDQxMjAxYjZlMGFkYmEwIiwidGFnIjoiIn0%3D'
     }
 
@@ -106,6 +107,59 @@ def ambil_total_bulanan_paralel(tahun):
             total_jml_sw_all_years += total_jml_sw
         
         return bulanan_totals, total_jml_sw_all_years
+
+# Fungsi untuk menghitung total jml_sw per bulan secara paralel
+def ambil_total_bulanan_loket(tahun):
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for bulan in range(1, 13):
+            start_date = f"01-{bulan:02d}-{tahun}"
+            end_date = f"31-{bulan:02d}-{tahun}"  # Memastikan batas akhir bulan tetap 31
+
+            url = f'https://ceri.jasaraharja.co.id/monitoring/swdkllj/datatables/{start_date}_{end_date}_0400001_1?_=1733116412480'
+            futures.append(executor.submit(ambil_data, url))  # Menjalankan ambil_data secara paralel
+        
+        # Mengambil hasil secara paralel
+        bulanan_totals = []
+        total_jml_sw_all_years = 0
+        for future in futures:
+            result, total_jml_sw = future.result()
+            
+            # Pastikan result tidak None sebelum slicing
+            if result is not None:
+                limited_result = result[:31]  # Ambil hanya 31 data pertama
+                
+                # Hitung ulang total_jml_sw dari data yang sudah dibatasi
+                total_jml_sw = sum(item['jml_sw'] for item in limited_result)
+
+            # Simpan hasil yang sudah difilter
+            bulanan_totals.append(total_jml_sw if total_jml_sw is not None else 0)
+            total_jml_sw_all_years += total_jml_sw if total_jml_sw is not None else 0
+
+        return bulanan_totals, total_jml_sw_all_years
+
+
+# Fungsi untuk menghitung total jml_sw per bulan secara paralel
+def ambil_total_bulanan_per_kantor(tahun, kode_kantor_jr):
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for bulan in range(1, 13):
+            start_date = f"01-{bulan:02d}-{tahun}"
+            end_day = monthrange(tahun, bulan)[1]  # Mendapatkan tanggal akhir yang tepat untuk bulan tersebut
+            end_date = f"{end_day}-{bulan:02d}-{tahun}"
+            url = f'https://ceri.jasaraharja.co.id/monitoring/swdkllj/datatables/{start_date}_{end_date}_{kode_kantor_jr}_2?_=1731895548035'
+            
+            futures.append(executor.submit(ambil_data, url))  # Menjalankan ambil_data secara paralel
+        
+        # Mengambil hasil secara paralel
+        bulanan_totals = []
+        total_jml_sw_all_years = 0
+        for future in futures:
+            result, total_jml_sw = future.result()
+            bulanan_totals.append(total_jml_sw if total_jml_sw is not None else 0)
+            total_jml_sw_all_years += total_jml_sw
+        
+        return bulanan_totals, total_jml_sw_all_years
     
 def ambil_total_bulanan_paralel_purwokerto(tahun):
     with ThreadPoolExecutor() as executor:
@@ -163,7 +217,6 @@ def gabungkan_data_per_kantor(data_2023, data_2024):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
     # Ambil total bulanan dan tahunan
     bulanan_2023, total_2023 = ambil_total_bulanan_paralel(previous_year)
     bulanan_2024, total_2024 = ambil_total_bulanan_paralel(current_year)
@@ -185,6 +238,16 @@ def index():
             end_date_2023 = session.get('end_date_2023', '2023-12-31')
             start_date_2024 = session.get('start_date_2024', '2024-01-01')
             end_date_2024 = session.get('end_date_2024', '2024-12-31')
+            
+            session['tahun_start_date_2023'] = start_date_2023.split('-')[0]
+            
+            session['tahun_start_date_2024'] = start_date_2024.split('-')[0]
+            
+            
+            tahun_start_date_2023 = session.get('tahun_start_date_2023')
+            
+            tahun_start_date_2024 = session.get('tahun_start_date_2024')
+            
 
             # URL API untuk data 2023 dan 2024
             url_2023 = f'https://ceri.jasaraharja.co.id/monitoring/swdkllj/datatables/{start_date_2023}_{end_date_2023}_0400001_1?_=1731895548035'
@@ -193,30 +256,57 @@ def index():
             # Ambil data API
             data_2023, total_jml_sw_2023 = ambil_data(url_2023)
             data_2024, total_jml_sw_2024 = ambil_data(url_2024)
+            
+            session['data_2023'] = data_2023  # Simpan ke session
+            session['data_2024'] = data_2024  # Simpan ke session
 
             # Gabungkan data per kantor
             data_kantor = gabungkan_data_per_kantor(data_2023, data_2024)
+            session['data_kantor'] = data_kantor  # Simpan ke session
+            # Hitung kabeh_sw_2023 dan kabeh_sw_2024
+            kabeh_sw_2023 = 0
+            kabeh_sw_2024 = 0
+            
+            for item in data_kantor[:37]:
+                kabeh_sw_2023 += item['total_sw_2023']
+                kabeh_sw_2024 += item['total_sw_2024']
+            
+            kabeh_diff = kabeh_sw_2024 - kabeh_sw_2023
+            kabeh_percent_change = (kabeh_diff / kabeh_sw_2023) * 100 if kabeh_sw_2023 != 0 else 0
 
-            return render_template('index.html', data_2023=data_2023, data_2024=data_2024,data_kantor=data_kantor, total_jml_sw_2023=total_jml_sw_2023,
-                                   total_jml_sw_2024=total_jml_sw_2024, start_date_2023=start_date_2023,
-                                   end_date_2023=end_date_2023, start_date_2024=start_date_2024, 
-                                   end_date_2024=end_date_2024, bulanan_2023=bulanan_2023, total_2023=total_2023, current_year=current_year, previous_year=previous_year,
-                                   bulanan_2024=bulanan_2024, total_2024=total_2024)
+            session['kabeh_sw_2023'] = kabeh_sw_2023  # Simpan ke session
+            session['kabeh_sw_2024'] = kabeh_sw_2024  # Simpan ke session
+            session['kabeh_diff'] = kabeh_diff  # Simpan ke session
+            session['kabeh_percent_change'] = kabeh_percent_change  # Simpan ke session
+            return render_template('index.html', data_2023=data_2023, data_2024=data_2024, data_kantor=data_kantor, 
+                                   total_jml_sw_2023=total_jml_sw_2023, total_jml_sw_2024=total_jml_sw_2024, 
+                                   start_date_2023=start_date_2023, end_date_2023=end_date_2023, 
+                                   start_date_2024=start_date_2024, end_date_2024=end_date_2024, 
+                                   bulanan_2023=bulanan_2023, total_2023=total_2023, 
+                                   bulanan_2024=bulanan_2024, total_2024=total_2024, 
+                                   kabeh_sw_2023=kabeh_sw_2023, kabeh_sw_2024=kabeh_sw_2024, 
+                                   kabeh_diff=kabeh_diff, kabeh_percent_change=kabeh_percent_change, current_year=current_year, previous_year=previous_year, tahun_start_date_2023=tahun_start_date_2023, tahun_start_date_2024=tahun_start_date_2024)
 
     return render_template('index.html', data_kantor=data_kantor, data_2023=None, data_2024=None, 
-                           bulanan_2023=bulanan_2023, total_2023=total_2023, bulanan_2024=bulanan_2024, current_year=current_year, previous_year=previous_year, total_2024=total_2024)
+                           bulanan_2023=bulanan_2023, total_2023=total_2023, bulanan_2024=bulanan_2024, current_year=current_year, previous_year=previous_year, total_2024=total_2024, tahun_start_date_2023=previous_year, tahun_start_date_2024=current_year)
 
 
 @app.route('/halaman_tertuju/<kode_kantor_jr>', methods=['GET', 'POST'])
 def halaman_tertuju(kode_kantor_jr):
     kantor_jr_query = request.args.get('kantor_jr', kode_kantor_jr)
     # Mendapatkan tanggal mulai dan akhir untuk dua rentang (2023 dan 2024)
+    
+    bulanan_2023, total_2023 = ambil_total_bulanan_per_kantor(previous_year, kode_kantor_jr)
+    bulanan_2024, total_2024 = ambil_total_bulanan_per_kantor(current_year, kode_kantor_jr)
+    
     # Ambil tanggal dari session
     start_date_2023 = session.get('start_date_2023', '2023-01-01')
     end_date_2023 = session.get('end_date_2023', '2023-12-31')
     start_date_2024 = session.get('start_date_2024', '2024-01-01')
     end_date_2024 = session.get('end_date_2024', '2024-12-31')
 
+    tahun_start_date_2023 = session.get('tahun_start_date_2023')
+    tahun_start_date_2024 = session.get('tahun_start_date_2024')
 
     # URL API untuk data 2023 dan 2024 dengan kode_kantor_jr
     url_2023 = f'https://ceri.jasaraharja.co.id/monitoring/swdkllj/datatables/{start_date_2023}_{end_date_2023}_{kode_kantor_jr}_2?_=1731895548035'
@@ -228,6 +318,8 @@ def halaman_tertuju(kode_kantor_jr):
 
     # Gabungkan data per kantor
     data_kantor = gabungkan_data_per_kantor(data_2023, data_2024)
+    
+
 
     # Menghitung perbedaan dan perubahan persentase
     diff = total_jml_sw_2024 - total_jml_sw_2023
@@ -235,14 +327,43 @@ def halaman_tertuju(kode_kantor_jr):
 
     return render_template('halaman_tertuju.html',
                            data_kantor=data_kantor,
-                           total_jml_sw_2023=total_jml_sw_2023,
+                           total_jml_sw_2023=total_jml_sw_2023,bulanan_2023=bulanan_2023, total_2023=total_2023, bulanan_2024=bulanan_2024, total_2024=total_2024,
                            total_jml_sw_2024=total_jml_sw_2024,
                            start_date_2023=start_date_2023,
                            end_date_2023=end_date_2023,
                            start_date_2024=start_date_2024,
                            end_date_2024=end_date_2024,
                            diff=diff, current_year=current_year, previous_year=previous_year,
-                           percent_change=percent_change, kantor_jr=kantor_jr_query)
+                           percent_change=percent_change, kantor_jr=kantor_jr_query, tahun_start_date_2023=tahun_start_date_2023, tahun_start_date_2024=tahun_start_date_2024)
+
+
+@app.route('/halaman_loket')
+def halaman_loket():
+    # Pastikan data_kantor tersedia di sesi atau global state sebelum dipanggil
+    data_kantor_terbatas = session.get('data_kantor', [])[:37]  # Ambil 37 data pertama
+    
+    bulanan_2023, total_2023 = ambil_total_bulanan_loket(previous_year)
+    bulanan_2024, total_2024 = ambil_total_bulanan_loket(current_year)
+
+    
+    kabeh_sw_2023 = session.get('kabeh_sw_2023')
+    kabeh_sw_2024 = session.get('kabeh_sw_2024')
+    kabeh_diff = session.get('kabeh_diff')
+    kabeh_percent_change = session.get('kabeh_percent_change')
+    
+    start_date_2023 = session.get('start_date_2023', '2023-01-01')
+    end_date_2023 = session.get('end_date_2023', '2023-12-31')
+    start_date_2024 = session.get('start_date_2024', '2024-01-01')
+    end_date_2024 = session.get('end_date_2024', '2024-12-31')
+    
+    tahun_start_date_2023 = session.get('tahun_start_date_2023')
+    tahun_start_date_2024 = session.get('tahun_start_date_2024')
+    
+    
+    return render_template('halaman_loket.html', data_kantor_terbatas=data_kantor_terbatas, start_date_2023=start_date_2023, bulanan_2024=bulanan_2024, total_2024=total_2024,
+                           end_date_2023=end_date_2023, current_year=current_year, previous_year=previous_year,
+                           start_date_2024=start_date_2024, bulanan_2023=bulanan_2023, total_2023=total_2023,
+                           end_date_2024=end_date_2024, total_jml_sw_2023_filtered=kabeh_sw_2023, total_jml_sw_2024_filtered=kabeh_sw_2024, kabeh_diff=kabeh_diff, kabeh_percent_change=kabeh_percent_change, tahun_start_date_2023=tahun_start_date_2023, tahun_start_date_2024=tahun_start_date_2024)
 
 
 if __name__ == '__main__':
